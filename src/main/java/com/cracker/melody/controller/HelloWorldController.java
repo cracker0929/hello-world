131c2e2636fb92434c147ade355c98716a8bc6f9
cracker0929 <42813984@qq.com>
2019-04-03 17:00:50 +0800

    加入swagger文檔組件，方便接口測試和回調聯調。

--- src/main/java/com/cracker/melody/controller/HelloWorldController.java
+++ src/main/java/com/cracker/melody/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.cracker.melody.model.User;
 import com.cracker.melody.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +15,13 @@ import java.util.List;
 /**
  * controller Test
  */
-@RestController
+    @Api(description = "controller Demo")
+    @RestController
     public class HelloWorldController {
       @Autowired
       private UserService userServiceImplment;
-        @RequestMapping("/hello")
+        @RequestMapping("/getUserList")
+        @ApiOperation(value = "查詢所有用戶信息", notes = "查询所有用戶信息")
         public List<User> hello() {
            /* Wrapper<User> wrapper = new EntityWrapper<>();
             ((EntityWrapper<User>) wrapper).setEntity(new User());*/
